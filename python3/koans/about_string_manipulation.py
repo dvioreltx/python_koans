06fbeb9adabf46f03d10c53e5a7f1015ab36bb87
Danny Viorel <dviorel@inmarket.com>
2020-01-13 17:48:00 -0400

    Lesson about String manipulation avance

--- python3/koans/about_string_manipulation.py
+++ python3/koans/about_string_manipulation.py
@@ -34,13 +34,13 @@ class AboutStringManipulation(Koan):
         self.assertEqual("a", string[1])
 
     def test_single_characters_can_be_represented_by_integers(self):
-        self.assertEqual(__, ord('a'))
-        self.assertEqual(__, ord('b') == (ord('a') + 1))
+        self.assertEqual(97, ord('a'))
+        self.assertEqual(True, ord('b') == (ord('a') + 1))
 
     def test_strings_can_be_split(self):
         string = "Sausage Egg Cheese"
         words = string.split()
-        self.assertListEqual([__, __, __], words)
+        self.assertListEqual(["Sausage", "Egg", "Cheese"], words)
 
     def test_strings_can_be_split_with_different_patterns(self):
         import re #import python regular expression library
@@ -57,8 +57,8 @@ class AboutStringManipulation(Koan):
     def test_raw_strings_do_not_interpret_escape_characters(self):
         string = r'\n'
         self.assertNotEqual('\n', string)
-        self.assertEqual(__, string)
-        self.assertEqual(__, len(string))
+        self.assertEqual("\\n", string)
+        self.assertEqual(2, len(string))
 
         # Useful in regular expressions, file paths, URLs, etc.
 
